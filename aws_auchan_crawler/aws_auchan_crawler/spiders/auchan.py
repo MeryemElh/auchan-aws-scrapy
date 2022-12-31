dd0ad36aae5f9a6d0d9baf3556711e599ec3300b
Meryem El Halfa <71674839+MeryemElh@users.noreply.github.com>
2022-12-31 03:27:38 +0000

    Error handling
    Able to handle (log if needed) if we are blocked by Auchan's ToS.

--- aws_auchan_crawler/aws_auchan_crawler/spiders/auchan.py
+++ aws_auchan_crawler/aws_auchan_crawler/spiders/auchan.py
@@ -1,6 +1,9 @@
 from scrapy import Spider, Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.http.response.html import HtmlResponse
+from scrapy.exceptions import IgnoreRequest
+from twisted.python.failure import Failure
+from twisted.internet.error import AlreadyCalled
 
 
 class AuchanSpider(Spider):
@@ -11,9 +14,14 @@ class AuchanSpider(Spider):
 
     def parse(self, response: HtmlResponse):
         for link in self.category_extractor.extract_links(response):
-            yield Request(link.url, callback=self.parse_category)
+            yield Request(link.url, callback=self.parse_category, errback=self.parse_category_err)
 
     def parse_category(self, response: HtmlResponse):
         #TODO: Parse sub-categories
         with open("text.txt", "a+", encoding="utf-8") as f:
             f.write(f"{response.url}\n")
+
+    def parse_category_err(self, failure: Failure):
+        if failure.check(IgnoreRequest):
+            #TODO: log or handle the fact that auchan blocked the scraper
+            pass
