68f38710e4c0b63394d0653bfec38d37f216968e
Meryem El Halfa <71674839+MeryemElh@users.noreply.github.com>
2022-12-31 03:25:48 +0000

    Category retrievement
    Retrieves the categories links and parses them in another method.
    For now only write them down in a temp file.
    
    NB: Auchan's Robots.txt doesn't allow some categories to be scraped (Promos at the time of the test for example). We can ignore it by setting the ROBOTSTXT_OBEY variable in settings.py to False but it would violate Auchan ToS (Terms of Service) and we could be open to a lawsuit however unlikely as it is.

--- aws_auchan_crawler/aws_auchan_crawler/spiders/auchan.py
+++ aws_auchan_crawler/aws_auchan_crawler/spiders/auchan.py
@@ -1,10 +1,19 @@
-import scrapy
+from scrapy import Spider, Request
+from scrapy.linkextractors import LinkExtractor
+from scrapy.http.response.html import HtmlResponse
 
 
-class AuchanSpider(scrapy.Spider):
+class AuchanSpider(Spider):
     name = 'auchan'
     allowed_domains = ['auchan.fr']
-    start_urls = ['http://auchan.fr/']
+    start_urls = ['http://www.auchan.fr/']
+    category_extractor = LinkExtractor(restrict_css=".navigation-node")
 
-    def parse(self, response):
-        pass
+    def parse(self, response: HtmlResponse):
+        for link in self.category_extractor.extract_links(response):
+            yield Request(link.url, callback=self.parse_category)
+
+    def parse_category(self, response: HtmlResponse):
+        #TODO: Parse sub-categories
+        with open("text.txt", "a+", encoding="utf-8") as f:
+            f.write(f"{response.url}\n")
